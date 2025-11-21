433d9cbb26bc5405bc1711e7469df044645f95ab
onsmart-admin <maicon.nunes@onsmart.com.br>
2025-11-21 17:46:57 -0300

    EvoulionAPI old dependencies removed from branch Mateus

--- api/evolution-webhook.js
+++
@@ -1,7 +0,0 @@
-// Endpoint removido
-export default async function handler(req, res) {
-  return res.status(404).json({ 
-    error: 'Endpoint desabilitado',
-    message: 'Evolution API foi removido do projeto.'
-  });
-}
--- src/components/admin/EvolutionApiConfig.tsx
+++
@@ -1,8 +0,0 @@
-// Componente removido
-import React from 'react';
-
-const EvolutionApiConfig: React.FC = () => {
-  return null;
-};
-
-export default EvolutionApiConfig;
